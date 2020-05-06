90fa2bada3cb544067e8eb0497dce17c6a103655
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2020-05-06 20:17:46 +0200

    small tweak to complex resistivity data

--- CR/synthetic_modeling/generate_data.py
+++ CR/synthetic_modeling/generate_data.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-r"""
-Complex-valued electrical modeling
-----------------------------------
-
-In this example an electrical complex-valued forward modeling is conducted. The
-use of complex resistivities implies an out-of-phase polarization response of
-the subsurface, commonly being measured in the frequency domain as complex
-resistivity (CR), or, if multiple frequencies are measured, as the spectral
-induced polarization (SIP). Please note that the time-domain induced
-polarization (TDIP) is a compound signature of a wide range of frequencies.
-
-It is common to parameterize the complex resistivities using magnitude (in
-:math:`\Omega m`) and phase :math:`\phi` (in mrad), although the PyGimli
-forward operator takes real and imaginary parts.
-"""
-# sphinx_gallery_thumbnail_number = 5
 import numpy as np
 import matplotlib.pylab as plt
 
@@ -41,11 +25,15 @@ scheme.set('k', [1 for x in range(scheme.size())])
 world = mt.createWorld(
     start=[-55, 0], end=[105, -80], worldMarker=True)
 
-polarizable_anomaly = mt.createCircle(
-    pos=[40, -7], radius=5, marker=2
+conductive_anomaly = mt.createCircle(
+    pos=[10, -7], radius=5, marker=2
 )
 
-plc = mt.mergePLC((world, polarizable_anomaly))
+polarizable_anomaly = mt.createCircle(
+    pos=[40, -7], radius=5, marker=3
+)
+
+plc = mt.mergePLC((world, conductive_anomaly, polarizable_anomaly))
 
 # local refinement of mesh near electrodes
 for s in scheme.sensors():
@@ -66,22 +54,9 @@ pg.show(mesh)
 rhomap = [
     [1, pg.utils.complex.toComplex(100, 0 / 1000)],
     # Magnitude: 50 ohm m, Phase: -50 mrad
-    [2, pg.utils.complex.toComplex(50, -50 / 1000)],
+    [2, pg.utils.complex.toComplex(50, 0 / 1000)],
+    [3, pg.utils.complex.toComplex(100, -50 / 1000)],
 ]
-
-# For visualization, map the rhomap into the actual mesh, resulting in a rho
-# vector with a complex resistivity associated with each mesh cell.
-rho = pg.solver.parseArgToArray(rhomap, mesh.cellCount(), mesh)
-fig, axes = plt.subplots(2, 2, figsize=(16 / 2.54, 16 / 2.54))
-pg.show(mesh, data=np.real(rho), ax=axes[0, 0], label=r"$\rho'~[\Omega m]$")
-pg.show(mesh, data=np.imag(rho), ax=axes[0, 1], label=r"$\rho''~[\Omega m]$")
-pg.show(mesh, data=np.abs(rho), ax=axes[1, 0], label=r"$|\rho|~[\Omega m]$")
-pg.show(
-    mesh, data=np.arctan2(np.imag(rho), np.real(rho)),
-    ax=axes[1, 1], label=r"$\phi$ [mrad]"
-)
-fig.tight_layout()
-fig.show()
 
 ###############################################################################
 # Do the actual forward modeling
